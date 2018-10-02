4825c89003a6fddbf87ae6a689a5ea99a7f2d8fc
Nattawut212 <31862875+Nattawut212@users.noreply.github.com>
2018-10-03 05:11:52 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+﻿<table width=100% bgcolor=#9900CC><tr><td>
+<center>
+<span style="font-size:20px;text-align:center;">
+โทร 081-123-3456 </td>
+</center>
+</span>
+</td></tr></table>
+</body></html>
+
---
+++ header.php
@@ -0,0 +1,12 @@
+<html><head><title>Nattawut</title></head>
+<meta http-equiv="content-type" content="text/html;charset=tis-620"/>
+<body bgcolor=pink>
+<table width=100% bgcolor=#66FFFF><tr><td>
+<center>
+<span style="font-size:30px;">Hello Welcome</span>
+</td></tr></table>
+<table width=100% bgcolor=#dddddd><tr>
+<td><a href=myweb.php>Home</a></td>
+</center>
+
+</tr></table>
